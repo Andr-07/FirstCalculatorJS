131d0912c4abb148a3e46c7a8f983ede431b1636
Andrei Lubochnikov <46841332+Andr-07@users.noreply.github.com>
2019-04-13 17:06:49 +0300

    All files

---
+++ main.js
@@ -0,0 +1,56 @@
+let numbers = document.querySelectorAll(".numbers");
+let operations = document.querySelectorAll(".operations");
+let tableButtons = document.querySelector('.tableButtons')
+let inputWindow = document.querySelector("#window");
+let equal = document.querySelector(".equal");
+let clear = document.querySelector(".clear");
+let dot = document.querySelector(".dot");
+
+let key = false;
+let num1, num2;
+let numberOfButton = "";
+let operationOfButton = "";
+
+
+tableButtons.addEventListener('click', function(event) {
+
+    switch (event.target.className) {
+//  **** Формирование чисел **** /
+        case "numbers":
+            if (key === false) {
+                    numberOfButton += event.target.innerText;
+                    num1 = numberOfButton;
+                    inputWindow.value = num1;
+                } else {
+                    inputWindow.value = "";
+                    numberOfButton += event.target.innerText;
+                    num2 = numberOfButton;
+                    inputWindow.value = num2;
+                }
+            break;
+
+  // ***** Форимирование операций *****  //          
+        case "operations":
+            key = true; // Маркер операций
+            if (num2) {
+                num1 = counting(num1, operationOfButton, num2);
+                inputWindow.value = num1;
+                num2 = undefined;
+                operationOfButton = event.target.innerText;              
+            }
+            else operationOfButton = event.target.innerText;
+            numberOfButton = "";
+            break;
+
+        case "equal":
+            inputWindow.value = counting(num1, operationOfButton, num2);          
+            numberOfButton = "";
+            key=false;
+            num2 = undefined;
+            break;
+
+        case "clear":
+            clearAll();
+            break;
+    }
+});
